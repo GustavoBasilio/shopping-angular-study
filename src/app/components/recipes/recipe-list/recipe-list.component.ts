c7b25fbcbe01fc87c12315401cdbb073a2d55736
Gustavo Basilio <gusbpb@gmail.com>
2018-12-19 23:21:49 -0200

    fix(master): Fixed components folders structure

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../../../models/recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://via.placeholder.com/50x50')
+  ];
   constructor() { }
 
   ngOnInit() {
